1b4ee9102965981c63a9fba34c4764f671e250df
Laura Lindzey <llindzey@whoi.edu>
2018-10-11 18:50:42 -0400

    Adding license headers to C++ files

--- LICENSES/bsd_license.cpp
+++ LICENSES/bsd_license.cpp
@@ -27,21 +27,3 @@
 * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 * POSSIBILITY OF SUCH DAMAGE.
 */
-/*
- * Fofonoff depth equations
- * Now including an Inverse!
- *
- * 2017 Nov 22
- *
- * Ian Vaughn
- * ivaughn@whoi.edu
- *
- */
-
-#pragma once
-
-#include <math.h>
-
-extern const double ALVIN_DEPTH_LAT;
-extern double fofonoff_depth(double pressure_dbar, double latitude_deg);
-extern double fofonoff_pressure(double depth_m, double latitude_deg);
