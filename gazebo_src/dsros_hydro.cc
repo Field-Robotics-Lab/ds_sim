4afc81a75b42d4f9a66583150c45cbda5f068552
Ian Vaughn <ivaughn@whoi.edu>
2019-01-31 09:51:02 -0500

    Basic buoyancy working

--- gazebo_src/dsros_hydro.cc
+++ gazebo_src/dsros_hydro.cc
@@ -44,12 +44,37 @@ void DsrosHydro::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf) {
   double mass = buoy->Get<double>("mass");
   grav_force_world = mass * (_parent->GetWorld()->Gravity());
 
+  body_link->SetGravityMode(false);
+
   // /////////////////////////////////////////////////////////////////////// //
-  GZ_ASSERT(_sdf->HasElement("buoyancy"), "DsrosHydro MUST specify a drag element");
+  GZ_ASSERT(_sdf->HasElement("drag"), "DsrosHydro MUST specify a drag element");
   sdf::ElementPtr drag = _sdf->GetElement("drag");
 
+  // this vector has to stay relative to the link origin, because that
+  // function we use to apply the relative force is relative to the link origin.
+  // Unlike ALL THE OTHER apply force functions.  Gazebo never met a coordinate system
+  // it could get documented.
+  if (!drag->HasElement("center")) {
+    gzwarn <<"ds_hydro plugin for link " <<body_link->GetName()
+           <<" does not specify a center of hydrodynamic force; assuming Center of Mass" <<std::endl;
+    drag_center_body = body_link->GetInertial()->GetCoG();
+  } else {
+    drag_center_body = loadVector(drag->GetElement("center"));
+  }
 
-  body_link->SetGravityMode(false);
+  if (drag->HasElement("linear")) {
+    drag_lin_coeff = loadMatrix(drag->GetElement("linear"));
+  } else {
+    gzwarn <<"ds_hydro plugin for link " <<body_link->GetName()
+           <<" does not specify linear drag matrix! (MAY CAUSE INSTABILITY!)" <<std::endl;
+  }
+
+  if (drag->HasElement("quadratic")) {
+    drag_lin_coeff = loadMatrix(drag->GetElement("quadratic"));
+  } else {
+    gzwarn <<"ds_hydro plugin for link " <<body_link->GetName()
+           <<" does not specify quadratic drag matrix!" <<std::endl;
+  }
 
   // /////////////////////////////////////////////////////////////////////// //
   // Connect the update handler
@@ -74,7 +99,43 @@ math::Vector3 DsrosHydro::loadVector(sdf::ElementPtr sdf) {
 }
 
 Matrix6d DsrosHydro::loadMatrix(sdf::ElementPtr sdf) {
+  Matrix6d ret;
+  ret.m11 = math::Matrix3::ZERO;
+  ret.m12 = math::Matrix3::ZERO;
+  ret.m21 = math::Matrix3::ZERO;
+  ret.m22 = math::Matrix3::ZERO;
 
+  std::vector<std::string> axis_tags = {"x", "y", "z", "r", "p", "h"};
+
+  for (size_t i=0; i<3; i++) {
+    for (size_t j=0; j<3; j++) {
+      // the top-left segment
+      std::string tag11 = axis_tags[i]   + axis_tags[j];
+      std::string tag12 = axis_tags[i]   + axis_tags[j+3];
+      std::string tag21 = axis_tags[i+3] + axis_tags[j];
+      std::string tag22 = axis_tags[i+3] + axis_tags[j+3];
+
+      if (sdf->HasElement(tag11)) {
+        ret.m11[i][j] = sdf->Get<double>(tag11);
+      }
+      if (sdf->HasElement(tag12)) {
+        ret.m12[i][j] = sdf->Get<double>(tag12);
+      }
+      if (sdf->HasElement(tag21)) {
+        ret.m21[i][j] = sdf->Get<double>(tag21);
+      }
+      if (sdf->HasElement(tag22)) {
+        ret.m22[i][j] = sdf->Get<double>(tag22);
+      }
+    }
+  }
+
+  //gzmsg <<"M_11: " <<ret.m11 <<"\n";
+  //gzmsg <<"M_12: " <<ret.m12 <<"\n";
+  //gzmsg <<"M_21: " <<ret.m21 <<"\n";
+  //gzmsg <<"M_22: " <<ret.m22 <<"\n";
+
+  return ret;
 }
 
 void DsrosHydro::OnUpdate(const common::UpdateInfo& _info) {
@@ -87,18 +148,31 @@ void DsrosHydro::OnUpdate(const common::UpdateInfo& _info) {
   body_link->AddForceAtRelativePosition(buoy_force_world, buoy_center_com);
 
   // Add linear drag
-  // TODO
+  math::Vector3 vel_lin = body_link->GetRelativeLinearVel();
+  math::Vector3 vel_ang = body_link->GetRelativeAngularVel();
+
+  math::Vector3 drag_lin_force = drag_lin_coeff.m11 * vel_lin + drag_lin_coeff.m12 * vel_ang;
+  math::Vector3 drag_lin_torque = drag_lin_coeff.m21 * vel_lin + drag_lin_coeff.m22 * vel_ang;
+
+  body_link->AddLinkForce(-drag_lin_force, drag_center_body);
+  body_link->AddRelativeTorque(-drag_lin_torque);
 
   // Add quadratic drag
-  // TODO
+  math::Vector3 vel_lin2 = vel_lin*vel_lin;
+  math::Vector3 vel_ang2 = vel_ang*vel_ang;
+
+  math::Vector3 drag_quad_force = drag_quad_coeff.m11 * vel_lin2 + drag_quad_coeff.m12 * vel_ang2;
+  math::Vector3 drag_quad_torque = drag_quad_coeff.m21 * vel_lin2 + drag_quad_coeff.m22 * vel_ang2;
+
+  body_link->AddLinkForce(-drag_quad_force, drag_center_body);
+  body_link->AddRelativeTorque(-drag_quad_torque);
 
   // Add a phantom "added mass" term
   // TODO
 
-  // Check total force at the end
-  math::Vector3 total_force = body_link->GetWorldForce();
-  math::Vector3 total_torque = body_link->GetWorldTorque();
-  gzmsg <<"WORLD force: " <<total_force <<" " <<total_torque <<"\n";
+  // This KILLs performance
+  // check total forces at the end
+  //gzmsg <<"WORLD force: " <<body_link->GetWorldForce() <<" " <<body_link->GetWorldTorque() <<"\n";
 
 }
 
--- gazebo_src/dsros_hydro.hh
+++ gazebo_src/dsros_hydro.hh
@@ -10,13 +10,21 @@
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
+#include <gazebo/math/gzmath.hh>
 
 // Damn you vile Gazebo
 struct Matrix6d {
-  ignition::math::Matrix3d m11;
-  ignition::math::Matrix3d m12;
-  ignition::math::Matrix3d m21;
-  ignition::math::Matrix3d m22;
+  Matrix6d() {
+    m11 = gazebo::math::Matrix3::ZERO;
+    m12 = gazebo::math::Matrix3::ZERO;
+    m21 = gazebo::math::Matrix3::ZERO;
+    m22 = gazebo::math::Matrix3::ZERO;
+  }
+
+  gazebo::math::Matrix3 m11;
+  gazebo::math::Matrix3 m12;
+  gazebo::math::Matrix3 m21;
+  gazebo::math::Matrix3 m22;
 };
 
 namespace gazebo {
@@ -41,6 +49,8 @@ class DsrosHydro : public ModelPlugin {
   math::Vector3 grav_force_world;
 
   math::Vector3 drag_center_body; // center-of-mass relative
+  Matrix6d drag_lin_coeff;
+  Matrix6d drag_quad_coeff;
 
   math::Vector3 loadVector(sdf::ElementPtr sdf);
   Matrix6d loadMatrix(sdf::ElementPtr sdf);
